39c204f0d2a3dce399bdc299ae711a7f34a68e82
Erwane <erwanxavierxvi@gmail.com>
2021-02-10 23:47:26 +0100

    update configuration docker and database

---
+++ dump/db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `user` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `prenom` varchar(60),
+  `nom` varchar(60),
+  `email` varchar(60),
+  `age` int,
+  `color` varchar(60),
+  `password` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `user` (`nom`,`prenom`,`email`,`age`,`color`,`password`) VALUES('LEBERT','Nicolas','nicolas@gmail.com', 24, 'rouge', '56sd1g$s$') ;
+INSERT INTO `user` (`nom`,`prenom`,`email`,`age`,`color`,`password`) VALUES('XAVIER','Erwan','erwan@gmail.com', 24, 'rouge' ,'$erg$dsfv$ds$$') ;
+INSERT INTO `user` (`nom`,`prenom`,`email`,`age`,`color`,`password`) VALUES('COUTROT','Sylvain','sylvain@gmail.com', 24, 'rouge', '$sfcszr$f') ;
